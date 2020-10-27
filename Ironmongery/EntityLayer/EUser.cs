8b54ee75251c4ada198bb5e4b72fa566a389270b
Carlos Zuniga Rodriguez <chala926@gmail.com>
2020-10-26 21:22:02 -0600

    Connection to User and Product

---
+++ Ironmongery/EntityLayer/EUser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+    public class EUser
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Type { get; set; }
+        public bool Admin { get; set; }
+
+        public EUser()
+        {
+        }
+
+        public EUser(int id, string code, string username, string password, string type, bool admin)
+        {
+            Id = id;
+            Code = code;
+            Username = username;
+            Password = password;
+            Type = type;
+            Admin = admin;
+        }
+
+        public override string ToString()
+        {
+            return $"{Code} - {Username}";
+        }
+    }
+}
--- Ironmongery/ServiceLayer/Util.cs
+++ Ironmongery/ServiceLayer/Util.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace ServiceLayer
 {
     public class Util
     {
+        public void showError(string message, string title)
+        {
+            MessageBox.Show(message, title,
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
     }
 }
