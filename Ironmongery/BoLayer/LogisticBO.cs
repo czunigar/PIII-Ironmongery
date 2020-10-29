c349b3fdc5887ba3a4d47c67c3f5dc747cca8b98
Carlos Zuniga Rodriguez <chala926@gmail.com>
2020-10-28 22:21:45 -0600

    Entity Framework and connection of services and logistic

---
+++ Ironmongery/BoLayer/LogisticBO.cs
@@ -0,0 +1,45 @@
+﻿using EntityLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer;
+
+namespace BoLayer
+{
+    public class LogisticBO
+    {
+        /*Method to add all the travels from the database*/
+        private UserBO ubo;
+        public List<ELogistic> loadTravels(string filter)
+        {
+            List<ELogistic> travels = new List<ELogistic>();
+            ubo = new UserBO();
+            using(IRONMONGERYEntities db = new IRONMONGERYEntities())
+            {
+                var myList = from t in db.LOGISTICS.ToList()
+                             select new ELogistic
+                             {
+                                 Id = t.Id,
+                                 VehicleNumber = t.Vehicle_Number,
+                                 DriverId = t.DriverID.Value,
+                                 Driver = ubo.getUserById(t.DriverID.Value),
+                                 Active = t.Active
+                             };
+                if (!string.IsNullOrEmpty(filter))
+                {
+                    myList = myList.Where(t => t.VehicleNumber.Equals(filter));
+                }
+
+                foreach (var trav in myList)
+                {
+                    travels.Add(new ELogistic(trav.Id, trav.VehicleNumber, trav.DriverId,
+                        trav.Driver, trav.Active));
+                }
+
+                return travels;
+            }
+        }
+    }
+}
---
+++ Ironmongery/BoLayer/ServiceBO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityLayer;
+using DataLayer;
+
+namespace BoLayer
+{
+    public class ServiceBO
+    {
+        /*Method to add to a list all the services in the database*/
+        public List<EService> loadServices(string filter)
+        {
+            List<EService> services = new List<EService>();
+            using(IRONMONGERYEntities db = new IRONMONGERYEntities())
+            {
+                var myList = from s in db.SERVICES.ToList()
+                             select new EService
+                             {
+                                 Id = s.Id,
+                                 ServiceName = s.Service_Name,
+                                 Category = s.Category,
+                                 ServiceDescription = s.Service_Description,
+                                 Price = s.Price
+                             };
+
+                if (!string.IsNullOrEmpty(filter))
+                {
+                    myList = myList.Where(s => s.ServiceName.Equals(filter));
+                }
+
+                foreach (var serv in myList)
+                {
+                    services.Add(new EService(serv.Id, serv.ServiceName, serv.Category,
+                        serv.ServiceDescription, serv.Price));
+                }
+                return services;
+            }
+        } 
+    }
+}
